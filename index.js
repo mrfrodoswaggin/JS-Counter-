064c4bee916ae0698084680859793fd9b0787377
Mr. Frodo <68685379+mrfrodoswaggin@users.noreply.github.com>
2022-08-06 11:09:57 +0530

    Added the complete files.

---
+++ index.js
@@ -0,0 +1,34 @@
+// document.getElementById("count-el").innerText = 5
+
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+
+let count = 0
+
+function increment(){
+    count += 1
+    countEl.textContent = count
+}
+
+function decrement(){
+    count -= 1
+    countEl.textContent = count
+}
+
+function save(){
+    let msg = count + " - "
+    //console.log(msg)
+    saveEl.textContent += msg
+    countEl.textContent = 0
+    count = 0
+}
+
+
+
+
+
+
+
+
+
+
